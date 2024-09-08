f0dd9ba07414009d105a913d305589d0dc103c40
Jacob Powers <powersjcb@gmail.com>
2024-09-08 21:14:50 +0000

    changes required to make iphone builds work for dev

--- scripts/unthebenternify.sh
+++ scripts/unthebenternify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i '' -e 's/6YF6QJH524/GCH7VS5Y9R/g' ./Meshtastic.xcodeproj/project.pbxproj
-sed -i '' -e 's/thebentern.Meshtastic/gvh.Meshtastic/g' ./Meshtastic.xcodeproj/project.pbxproj
+sed -i '' -e 's/6YF6QJH524/LQPP7M7XNY/g' ./Meshtastic.xcodeproj/project.pbxproj
+sed -i '' -e 's/jpowers.Meshtastic/gvh.Meshtastic/g' ./Meshtastic.xcodeproj/project.pbxproj
