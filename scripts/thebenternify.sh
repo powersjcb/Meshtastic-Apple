e02d5320d5e09c7e5de113534c3f4f51e8184e90
Jacob Powers <powersjcb@gmail.com>
2024-09-08 21:53:06 +0000

    build tooling

--- scripts/thebenternify.sh
+++ scripts/thebenternify.sh
@@ -2,3 +2,7 @@
 
 sed -i '' -e 's/GCH7VS5Y9R/LQPP7M7XNY/g' ./Meshtastic.xcodeproj/project.pbxproj
 sed -i '' -e 's/gvh.Meshtastic/jpowers.Meshtastic/g' ./Meshtastic.xcodeproj/project.pbxproj
+
+# patch the entitlements file to remove carplay
+patch ./Meshtastic/Meshtastic.entitlements ./scripts/entitlements-build.patch
+
--- scripts/unthebenternify.sh
+++ scripts/unthebenternify.sh
@@ -2,3 +2,6 @@
 
 sed -i '' -e 's/6YF6QJH524/LQPP7M7XNY/g' ./Meshtastic.xcodeproj/project.pbxproj
 sed -i '' -e 's/jpowers.Meshtastic/gvh.Meshtastic/g' ./Meshtastic.xcodeproj/project.pbxproj
+
+# revert entitlement changes
+patch ./Meshtastic/Mestastic.entitlements ./scripts/entitlements-revert.patch
